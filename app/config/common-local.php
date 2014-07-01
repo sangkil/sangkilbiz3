23441962c19a5e877185cf55207c1491ce01ac7a
mdmunir <misbahuldmunir@gmail.com>
2014-07-01 13:43:18 +0700

    Prepare to public review

---
+++ app/config/common-local.php
@@ -0,0 +1,14 @@
+<?php
+
+return[
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=sangkilbiz3',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+            //'enableSchemaCache' => true,
+        ],
+    ],
+];
