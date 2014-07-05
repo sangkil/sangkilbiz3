11c07fe025b1813bcf69e760eaca002bdce78918
mujib masyhudi <masyhudi.melik@gmail.com>
2014-07-05 10:04:19 +0700

    add user-list action route: site/user-list

---
+++ app/views/site/user-list.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/**
+ * @var yii\web\View $this
+ * @var yii\data\ActiveDataProvider $dataProvider
+ * @var biz\master\models\ProductSearch $searchModel
+ */
+$this->title = 'Products';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="product-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?php //echo $this->render('_search', ['model' => $searchModel]);  ?>
+
+    <div class="pull-right">
+        <?= Html::a('', ['signup'], ['class' => 'btn btn-default glyphicon glyphicon-plus', 'title' => 'SignUp', 'style' => 'width:100%;']) ?>
+    </div>
+
+    <?=
+    GridView::widget([
+        'dataProvider' => $dataProvider,
+        'tableOptions' => ['class' => 'table table-striped'],
+        'layout' => '{items}{pager}',
+        //'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+            'username',
+            'email',
+            'role',
+            'status',
+            'created_at',
+            'updated_at'
+        ],
+    ]);
+    ?>
+</div>
