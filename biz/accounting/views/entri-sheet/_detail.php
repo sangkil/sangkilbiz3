95a227f3d8b03b976c284a2d1372c83951fa420f
mdmunir <misbahuldmunir@gmail.com>
2014-07-05 23:02:09 +0700

    accounting fix

--- biz/accounting/views/entri-sheet/_detail.php
+++ biz/accounting/views/entri-sheet/_detail.php
@@ -5,16 +5,16 @@ use yii\helpers\Html
 /* @var $model biz\accounting\models\EntriSheetDtl */
 /* @var $index string */
 ?>
-<td ><span class="serial"><?= $index ?></span></td>
+<td ><span class="serial"><?= $index+1 ?></span></td>
 <td><?=
-    Html::activeTextInput($model, "[$index]nm_esheet_dtl", [
+    Html::activeTextInput($model, "[$key]nm_esheet_dtl", [
         'style' => 'width:190px;',
     ])
     ?>
 </td>
 <td>
-    <?= Html::activeHiddenInput($model, "[$index]id_coa", ['class' => 'id_account']) ?>
-    <?= Html::activeTextInput($model, "[$index]idCoa[nm_account]", ['class' => 'nm_account', 'style' => 'width:260px']) ?>&nbsp;
+    <?= Html::activeHiddenInput($model, "[$key]id_coa", ['class' => 'id_account']) ?>
+    <?= Html::activeTextInput($model, "[$key]idCoa[nm_account]", ['class' => 'nm_account', 'style' => 'width:260px']) ?>&nbsp;
     <span class="cd_account"><?= \yii\helpers\ArrayHelper::getValue($model, 'idCoa.cd_account'); ?></span>
 </td>
 <td class="action">
