11dfa452825d5d0e3cc760bfa89630331546ce1d
MDMunir <misbahuldmunir@gmail.com>
2014-07-09 10:38:26 +0700

    Create web-local.php

---
+++ app/config/web-local.php
@@ -0,0 +1,21 @@
+<?php
+$config = [
+    'modules' => [
+    ],
+    'components' => [
+        'view'=>[
+//            'theme' => 'biz\adminlte\Theme'
+        ]
+    ]
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = 'yii\gii\Module';
+}
+
+return $config;
