6003aff3b0f17ddac762fa6850817d7a63097333
mdmunir <misbahuldmunir@gmail.com>
2014-08-30 07:46:21 +0700

    remove params

--- app/config/params.php
+++ app/config/params.php
@@ -1,26 +1,4 @@
 <?php
 
 return [
-    'adminEmail' => 'admin@example.com',
-    // Use for RecordLogger
-    'mdm.logger' => [
-        'storage' => [
-            'class' => 'mdm\logger\storages\MongoStorage',
-        ],
-        'attachTo' => ['db'], // set to false
-    ],
-    // sangkil biz
-    'sangkil.biz.app' => [
-    ],
-    'sangkil.biz.master' => [
-    ],
-    'sangkil.biz.purchase' => [
-    ],
-    'sangkil.biz.inventory' => [
-    ],
-    'sangkil.biz.sales' => [
-        'attach_client_behavior' => true, //
-    ],
-    'sangkil.biz.accounting' => [
-    ],
 ];
