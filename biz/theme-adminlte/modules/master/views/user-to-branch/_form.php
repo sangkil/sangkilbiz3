0a4bbd1b4c0a4d4449ddf62507cddc7102daf502
samsung-pc <samsung-pc@SangkilSoft>
2014-08-31 11:47:59 +0700

    update user2brach model and others

--- biz/theme-adminlte/modules/master/views/user-to-branch/_form.php
+++ biz/theme-adminlte/modules/master/views/user-to-branch/_form.php
@@ -4,6 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper;
 use biz\master\models\Branch;
+use app\models\User;
 
 /**
  * @var yii\web\View $this
@@ -19,7 +20,21 @@ use biz\master\models\Branch;
         <?= ''//$form->field($model, 'id_branch')->textInput() ?>
         <?= $form->field($model, 'id_branch')->dropDownList(ArrayHelper::map(Branch::find()->all(), 'id_branch', 'nm_branch')) ?>
 
-        <?= $form->field($model, 'id_user')->textInput() ?>
+        <?= ''//$form->field($model, 'id_user')->textInput() ?>
+        <?php
+            $listUser = User::find()->select('username')->asArray()->column();
+            $dUser = [];
+//            foreach ($listUser as $row) {
+//                $dUser[]=$row['username'];
+//            }
+            echo $form->field($model, 'nmUser')
+                ->widget('yii\jui\AutoComplete', [
+                    'options' => ['class' => 'form-control'],
+                    'clientOptions' => [
+                        'source' => $listUser,
+                    ]
+        ]);
+        ?>
     </div>
     <div class="box-footer">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
